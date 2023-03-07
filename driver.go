7ef2f27fe80d5341ffbda96326d7e428d1a81f1f
zhengyu <zhengyu@forwardx.com>
2023-03-07 22:58:26 +0800

    remove nacos

--- driver.go
+++ driver.go
@@ -3,11 +3,7 @@ package driver
 import (
 	"fmt"
 	"net/url"
-	"strconv"
 	"strings"
-
-	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/zeromicro/zero-contrib/zrpc/registry/nacos"
 
 	"github.com/zeromicro/zero-contrib/zrpc/registry/consul"
 
@@ -21,7 +17,6 @@ const (
 	kindEtcd   = "etcd"
 	kindDiscov = "discov"
 	kindConsul = "consul"
-	kindNacos  = "nacos"
 )
 
 type (
@@ -66,41 +61,6 @@ func (z *zeroDriver) RegisterService(target string, endpoint string) error {
 				"Protocol": "grpc",
 			},
 		})
-	case kindNacos:
-		// server
-		hostPort := strings.Split(u.Host, ":")
-		host := hostPort[0]
-		port, _ := strconv.ParseUint(hostPort[1], 10, 64)
-
-		// client
-		var namespaceId = "public"
-		var timeoutMs uint64 = 5000
-		var notLoadCacheAtStart = true
-		var logLevel = "debug"
-		if query.Get("namespaceId") != "" {
-			namespaceId = query.Get("namespaceId")
-		}
-		if query.Get("timeoutMs") != "" {
-			timeoutMs, _ = strconv.ParseUint(query.Get("timeoutMs"), 10, 64)
-		}
-		if query.Get("notLoadCacheAtStart") != "" && query.Get("notLoadCacheAtStart") == "false" {
-			notLoadCacheAtStart = false
-		}
-		if query.Get("logLevel") != "" {
-			logLevel = query.Get("logLevel")
-		}
-
-		sc := []constant.ServerConfig{
-			*constant.NewServerConfig(host, port),
-		}
-		cc := &constant.ClientConfig{
-			NamespaceId:         namespaceId,
-			TimeoutMs:           timeoutMs,
-			NotLoadCacheAtStart: notLoadCacheAtStart,
-			LogLevel:            logLevel,
-		}
-		opts := nacos.NewNacosConfig(strings.TrimPrefix(u.Path, "/"), endpoint, sc, cc)
-		return nacos.RegisterService(opts)
 	default:
 		return fmt.Errorf("unknown scheme: %s", u.Scheme)
 	}
@@ -118,7 +78,7 @@ func (z *zeroDriver) ParseServerMethod(uri string) (server string, method string
 
 	}
 	//resolve gozero consul wait=xx url.Parse no standard
-	if (strings.Contains(uri, kindConsul) || strings.Contains(uri, kindNacos)) && strings.Contains(uri, "?") {
+	if (strings.Contains(uri, kindConsul)) && strings.Contains(uri, "?") {
 		tmp := strings.Split(uri, "?")
 		sep := strings.IndexByte(tmp[1], '/')
 		if sep == -1 {
